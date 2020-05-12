855f0886f35ec9748e5d2d1c81127a5b9f42db0e
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-12 21:09:19 +0300

    Add project files.

---
+++ Northwind.Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Entities.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public Int16 UnitsInStock { get; set; }
+
+    }
+}
